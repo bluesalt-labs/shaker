cc3895c4c497288c48ef2a2f9a78ebf276813a67
Luke Sontrop <luke@bluesaltlabs.com>
2016-01-22 13:37:19 -0800

    Fixes Project name in composer file. Adds test base controller for crystals

---
+++ app/Http/Controllers/CrystalController.php
@@ -0,0 +1,22 @@
+<?php // this is not in the correct directory. Figure out where this should go
+
+namespace App\Http\Controllers;
+
+class CrystalController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->crystals = array();
+
+    }
+
+    public function loadCrystals() {
+        // Searches /crystals directory for crystals to load
+    }
+
+    //
+}
